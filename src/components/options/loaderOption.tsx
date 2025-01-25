424524a83c5264bb5db4e91f345c2d90794c3c0a
Esteban Rojas <esteban.jnf@gmail.com>
2025-01-25 20:05:30 +0100

    Adding custom CSS styles

---
+++ src/components/options/loaderOption.tsx
@@ -0,0 +1,13 @@
+import "./loaderOption.css"
+// components/Loader.tsx
+
+import React from "react";
+import "../styles/loader.css"; // Asegúrate de tener el archivo de estilos
+
+const Loader: React.FC = () => {
+  return (
+    <div className="loader"></div>
+  );
+};
+
+export default Loader;
