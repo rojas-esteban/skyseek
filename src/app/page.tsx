9d7a7dbd420793e5bf444ef236e75e7d09552da4
Esteban Rojas <esteban.jnf@gmail.com>
2025-01-25 20:23:00 +0100

    img patagonia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="grid bg-deepTeal grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 sm:p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)] ">
-     
-        <header>
-            <Image
-              src="/patagonia-banner-1.jpg"
-              alt="Descripción de la imagen"
-              width={200}
-              height={200}
-              className="rounded-lg shadow-xl mt-4  border-green-200 w-full h-auto object-cover sm:max-w-md"
-            />
-            
-        </header>
-        
-        
-      
+      <div className="grid bg-deepTeal grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 sm:p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)] ">     
 
         <main className="flex flex-col row-start-2 justify-center items-center h-full w-full">
           <Image
@@ -32,6 +18,13 @@ export default function Home() {
         </main>
 
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center w-full">
+        <Image
+              src="/patagonia-banner-1.jpg"
+              alt="Descripción de la imagen"
+              width={200}
+              height={200}
+              className="rounded-lg shadow-xl mt-4  border-green-200 w-full h-auto object-cover sm:max-w-md"
+            />
         </footer>
       </div>
     </>
