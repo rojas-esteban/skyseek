e7d695bd8e1a6a79bc69203ec77ca6db936001f0
Esteban Rojas <esteban.jnf@gmail.com>
2025-01-29 23:03:20 +0100

    v3

--- src/app/codeAi/page.tsx
+++ src/app/codeAi/page.tsx
@@ -83,7 +83,7 @@ const DeepSeekWidget = () => {
 
   return (
 
-    <div className="flex flex-col gap-8 p-8 mt-12">
+    <div className="flex flex-col gap-8  mb-12 p-8 mt-16">
       <h1 className="text-center font-semibold text-2xl m-4 sm:text-2xl"> Explications 💬, debug 🐞 et tout sur le code 💻 </h1>
           {/* <Image
             src="/453633559_4227827887443827_2217402644947367570_n.jpg"
